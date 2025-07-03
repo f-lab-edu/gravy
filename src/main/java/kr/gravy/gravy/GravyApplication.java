98136145060a33a7c5957e020a9dacff559a439f
Jackson <u_innovation@naver.com>
2025-07-03 12:29:00 +0900

    :sparkles: project init

---
+++ src/main/java/kr/gravy/gravy/GravyApplication.java
@@ -0,0 +1,13 @@
+package kr.gravy.gravy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GravyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/gravy/gravy/GravyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gravy.gravy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
